da85b8b6f4405c2c140cbe4eb07a76fa41632981
K4ZWRLD <94097110+K4ZWRLD@users.noreply.github.com>
2025-07-16 13:26:20 -0500

    Update spotify.js

--- utils/spotify.js
+++ utils/spotify.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const TOKENS_PATH = './spotify_tokens.json';
 
 function loadTokens() {
+  if (!fs.existsSync(TOKENS_PATH)) return {};
   return JSON.parse(fs.readFileSync(TOKENS_PATH));
 }
 
@@ -28,7 +29,10 @@ async function refreshAccessToken(userToken) {
 
   userToken.access_token = res.data.access_token;
   userToken.expires_at = Date.now() + res.data.expires_in * 1000;
-  saveTokens({ ...loadTokens(), [userToken.id]: userToken });
+
+  const tokens = loadTokens();
+  tokens[userToken.id] = userToken;
+  saveTokens(tokens);
 
   return userToken.access_token;
 }
@@ -50,30 +54,41 @@ async function getCurrentTrack(tokenObj) {
   if (!res.data || !res.data.item) return null;
 
   const item = res.data.item;
+
   return {
     name: item.name,
     artist: item.artists.map(a => a.name).join(', '),
     album: item.album.name,
     image: item.album.images[0]?.url,
-    url: item.external_urls.spotify
+    url: item.external_urls.spotify,
+    progress_ms: res.data.progress_ms,
+    duration_ms: item.duration_ms,
+    is_playing: res.data.is_playing
   };
 }
 
-async function getRecentTracks(tokenObj) {
+async function getRecentTracks(tokenObj, limit = 5) {
   const accessToken = await getValidAccessToken(tokenObj.id, tokenObj);
 
-  const res = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=5', {
+  const res = await axios.get(`https://api.spotify.com/v1/me/player/recently-played?limit=${limit}`, {
     headers: { Authorization: `Bearer ${accessToken}` }
   });
 
-  return res.data.items.map(({ track }) => ({
+  return res.data.items.map(({ track, played_at }) => ({
     name: track.name,
     artist: track.artists.map(a => a.name).join(', '),
-    url: track.external_urls.spotify
+    album: track.album.name,
+    image: track.album.images[0]?.url,
+    url: track.external_urls.spotify,
+    played_at
   }));
 }
 
 module.exports = {
+  loadTokens,
+  saveTokens,
   getCurrentTrack,
-  getRecentTracks
+  getRecentTracks,
+  getValidAccessToken,
+  refreshAccessToken
 };
